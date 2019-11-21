6cc8cd7ba7fe8c9677988c75c5cc1f56dc7c56a2
zhangyongwang <20225614@qq.com>
2019-11-21 16:04:32 +0800

    完成登出逻辑

--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,24 +1,49 @@
 import {Button} from "antd";
 import * as React from "react";
+import axios from 'src/config/axios';
 
 interface IRouter {
     history: any;
 }
 
-class Index extends React.Component<IRouter> {
+interface IIndexState {
+    user: any
+}
+
+class Index extends React.Component<IRouter, IIndexState> {
     constructor(props: any) {
-        super(props)
+        super(props);
+        this.state = {
+            user: {}
+        }
     }
 
-    login = () => {
+    async componentWillMount() {
+        await this.getMe();
+    }
+
+    getMe = async () => {
+        try {
+            const response = await axios.get('me');
+            console.log(response);
+            this.setState({user: response.data})
+        } catch (e) {
+            // if (e.response.status === 401) {
+            //     this.props.history.push('/login')
+            // }
+        }
+    };
+
+    logout = () => {
+        localStorage.setItem('x-token', '');
         this.props.history.push('/login')
     };
 
     public render() {
         return (
             <div>
-                <h2>Index</h2>
-                <Button onClick={this.login}>登录</Button>
+                <p>欢迎，{this.state.user && this.state.user.account}</p>
+                <Button onClick={this.logout}>注销</Button>
             </div>
         )
     }
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
+// import {createBrowserHistory} from 'history';
 
+// const history = createBrowserHistory();
 const appID = "eAQ5N5BM2iodni56TESfj2SW";
 const appSecret = "Jq5sRZ5KBubwTBrGwYNhbsHi";
 
@@ -31,6 +33,10 @@ instance.interceptors.response.use(response => {
     }
     return response;
 }, error => {
+    if (error.response.status === 401) {
+        console.log("重定向");
+        window.location.href = '/login'
+    }
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);

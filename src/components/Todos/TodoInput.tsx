dac38ca218ab8d3153db68a790d0000cc13eeb3a
zhangyongwang <20225614@qq.com>
2019-11-26 20:43:49 +0800

    添加 addTodo 到 TodoIinput 组件

--- src/components/Todos/TodoInput.tsx
+++ src/components/Todos/TodoInput.tsx
@@ -1,12 +1,17 @@
 import {Icon, Input} from "antd";
 import * as React from "react";
+import {connect} from 'react-redux'
+import {addTodo} from "../../redux/actions";
+import axios from 'src/config/axios';
+
+// import todos from "../../redux/reducers/todos";
 
 interface ITodoInputState {
     description: string
 }
 
 interface ITodoInputProps {
-    addTodo: (params: any) => void;
+    addTodo: (payload: any) => any;
 }
 
 class TodoInput extends React.Component<ITodoInputProps, ITodoInputState> {
@@ -14,23 +19,31 @@ class TodoInput extends React.Component<ITodoInputProps, ITodoInputState> {
         super(props);
         this.state = {
             description: ''
-        }
+        };
+        console.log(this.props);
     }
 
-    addTodo = () => {
-        this.props.addTodo({description: this.state.description})
-        this.setState({description: ''})
-    };
     onKeyUp = (e: any) => {
         if (e.keyCode === 13 && this.state.description !== '') {
-            this.addTodo();
+            this.postTodo();
         }
     };
+
+    postTodo = async () => {
+        try {
+            const response = await axios.post('todos', {description: this.state.description});
+            this.props.addTodo(response.data.resource)
+        } catch (e) {
+            throw new Error(e)
+        }
+        this.setState({description: ''})
+    };
+
 
     render() {
         const {description} = this.state;
         const suffix = description ? <Icon type="enter"
-                                           onClick={this.addTodo}
+                                           onClick={this.postTodo}
                                            style={{color: 'rgba(0,0,0,.45)'}}/> : <span/>;
         return (
             <div className="TodoInput" id="TodoInput">
@@ -46,4 +59,13 @@ class TodoInput extends React.Component<ITodoInputProps, ITodoInputState> {
     }
 }
 
-export default TodoInput
+const mapStateToProps = (state: any, ownProps: any) => ({
+    ...ownProps
+});
+
+const mapDispatchToProps = {
+    addTodo
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoInput)
+
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import {connect} from 'react-redux'
+import {addTodo} from "../../redux/actions";
 import TodoInput from "src/components/Todos/TodoInput";
 import TodoItem from "src/components/Todos/TodoItem";
 import axios from 'src/config/axios';
@@ -83,7 +85,7 @@ class Todos extends React.Component<any, ITodosState> {
     render() {
         return (
             <div className="Todos" id="Todos">
-                <TodoInput addTodo={this.addTodo}/>
+                <TodoInput/>
                 <div className="todoLists">
                     {
                         this.unCompletedTodos.map(t =>
@@ -105,4 +107,17 @@ class Todos extends React.Component<any, ITodosState> {
     }
 }
 
-export default Todos
+const mapStateToProps = (state: any, ownProps: any) => ({
+    todos: state.todos,
+    ...ownProps
+});
+
+const mapDispatchToProps = {
+    addTodo
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todos)
+
+
+

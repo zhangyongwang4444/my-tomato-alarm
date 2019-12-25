ddc0bb35444589bfb556b347eda9028c7fe91d9e
zhangyongwang <20225614@qq.com>
2019-12-25 19:25:27 +0800

    1. 拆分 actions 2. get tomatoes 3. post tomatoes

--- src/components/Tomatoes/TomatoAction.tsx
+++ src/components/Tomatoes/TomatoAction.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 import {Button} from "antd";
-import axios from 'src/config/axios';
 
-class TomatoAction extends React.Component {
+interface ITomatoActionProps {
+    startTomato: () => void;
+    unfinishedTomato: any
+}
 
-    startTomato = async () => {
-        try {
-            const response = await axios.post('tomatoes', {duration: 1500000});
-            console.log(response.data);
-        } catch (e) {
-            throw new Error(e)
-        }
-    };
+
+class TomatoAction extends React.Component<ITomatoActionProps> {
+    constructor(props: ITomatoActionProps) {
+        super(props)
+    }
 
     public render() {
         return (
             <div className="TomatoAction" id="TomatoAction">
-                <Button className="startTomatoButton" onClick={this.startTomato}>开始番茄</Button>
+                <Button className="startTomatoButton" onClick={() => {
+                    this.props.startTomato()
+                }}>开始番茄</Button>
             </div>
         )
     }
--- src/components/Tomatoes/Tomatoes.tsx
+++ src/components/Tomatoes/Tomatoes.tsx
@@ -1,28 +1,65 @@
 import * as React from 'react';
-import {initTodos, updateTodo} from "../../redux/actions";
+import axios from "../../config/axios";
+import {initTomatoes, addTomato} from "../../redux/actions/tomatoes";
 import TomatoAction from "./TomatoAction";
 import {connect} from "react-redux";
 import './Tomatoes.scss'
 
+interface ITomatoesProps {
+    addTomato: (payload: any) => any
+    tomatoes: any[]
+}
 
-class Tomatoes extends React.Component {
+class Tomatoes extends React.Component<ITomatoesProps> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    componentDidMount(): void {
+        this.getTomatoes()
+    }
+
+    get unfinishedTomato() {
+        return this.props.tomatoes.filter(t => !t.description && !t.ended_at)[0]
+    }
+
+    getTomatoes = async () => {
+        try {
+            const response = await axios.get('tomatoes')
+            console.log(response);
+        } catch (e) {
+            throw new Error(e)
+        }
+    };
+
+
+    startTomato = async () => {
+        try {
+            const response = await axios.post('tomatoes', {duration: 1500000});
+            this.props.addTomato(response.data.resourse);
+        } catch (e) {
+            throw new Error(e)
+        }
+    };
+
     public render() {
         return (
             <div className="Tomatoes" id="Tomatoes">
-                <TomatoAction/>
+                <TomatoAction startTomato={this.startTomato} unfinishedTomato={this.unfinishedTomato}/>
             </div>
         )
     }
 }
+
 const mapStateToProps = (state: any, ownProps: any) => ({
     tomatoes: state.tomatoes,
     ...ownProps
 });
 
 const mapDispatchToProps = {
-    initTodos,
-    updateTodo
+    initTomatoes,
+    addTomato
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(Tomatoes);
+export default connect(mapStateToProps, mapDispatchToProps)(Tomatoes);
 
